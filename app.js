1460c4ea8d67702492d86bcb2226735e93486a8c
Brandon McIntosh <Brandon.L.Mcintosh@gmail.com>
2020-10-20 13:23:08 -0700

    Finished creating all classes from exercise

---
+++ app.js
@@ -0,0 +1,44 @@
+class Vehicle{
+    constructor(make, model, year){
+        this.make = make;
+        this.model = model; 
+        this.year = year;
+    }
+    honk(){
+        return 'Beep.';
+    }
+    toString(){
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}`;
+    }
+}
+
+class Car extends Vehicle{
+    constructor(make, model, year){
+        super(make, model, year);
+        this.numWheels = 4;
+    }
+}
+
+class Motorcycle extends Vehicle{
+    constructor(make, model, year){
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+    revEngine(){
+        return 'VROOM!!!';
+    }
+}
+
+class Garage{
+    constructor(capacity){
+        this.vehicles = [];
+        this.capacity = capacity;
+    }
+    add(vehicle){
+        this.vehicles.length < this.capacity ? 
+            vehicle instanceof Vehicle ?
+              (this.vehicles.push(vehicle), console.log('Vehicle Added!')) : console.log('Only vehicles are allowed in here!')
+            : console.log("Sorry, we're full.");
+
+    }
+}
